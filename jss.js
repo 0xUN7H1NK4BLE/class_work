c60bc00817451e330a39702cccc45ecbd1caca58
Anishsir404 <anishji0306@gmail.com>
2022-12-26 09:45:07 +0545

    js

---
+++ jss.js
@@ -0,0 +1,12 @@
+let obj = {
+    anish: 90,
+    anish1: 95,
+    anish2: 96,
+    anish3: 57,
+    anish5: 23
+}
+
+// For in loop
+for (let a in obj) {
+  console.log("Marks of " + a + " is " + obj[a])
+}
