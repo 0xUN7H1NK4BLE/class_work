054d77c08aacbe0a780973d2ab2bf800bf9ccfe9
Anishsir404 <anishji0306@gmail.com>
2022-12-26 09:57:10 +0545

    function

---
+++ function.js
@@ -0,0 +1,7 @@
+function average(x,y) {
+    return (x+y)/2
+}
+
+console.log("average of 5 and 6 is",average(5,6))
+console.log("average of 5 and 5 is",average(5,5))
+console.log("average of 5 and 8 is",average(5,8))
